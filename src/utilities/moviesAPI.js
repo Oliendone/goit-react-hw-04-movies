b645a7e0d8496b5c3ca5db4934f56f6d9bc2d1ab
Nikita <Zungarcheg@gmail.com>
2020-10-13 20:04:14 +0300

    fixes with axios

--- src/utilities/moviesAPI.js
+++ src/utilities/moviesAPI.js
@@ -1,36 +1,36 @@
 import axios from 'axios';
 
-const baseURL = 'https://api.themoviedb.org';
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
 const key = '8ca227215ef92806ff11aa15496e0565';
 
 const trendingMovies = () => {
   return axios
-    .get(`${baseURL}/3/trending/movie/day?api_key=${key}`)
+    .get(`trending/movie/day?api_key=${key}`)
     .then(response => response.data.results);
 };
 
 const particularMovie = id => {
   return axios
-    .get(`${baseURL}/3/movie/${id}?api_key=${key}`)
-    .then(responce => responce.data);
+    .get(`movie/${id}?api_key=${key}`)
+    .then(response => response.data);
 };
 
 const movieCast = id => {
   return axios
-    .get(`${baseURL}/3/movie/${id}/credits?api_key=${key}`)
-    .then(responce => responce.data.cast);
+    .get(`movie/${id}/credits?api_key=${key}`)
+    .then(response => response.data.cast);
 };
 
 const movieReviews = id => {
   return axios
-    .get(`${baseURL}/3/movie/${id}/reviews?api_key=${key}`)
-    .then(responce => responce.data);
+    .get(`movie/${id}/reviews?api_key=${key}`)
+    .then(response => response.data);
 };
 
 const moviesSearch = movie => {
   return axios
-    .get(`${baseURL}/3/search/movie?api_key=${key}&query=${movie}`)
-    .then(responce => responce.data.results);
+    .get(`search/movie?api_key=${key}&query=${movie}`)
+    .then(response => response.data.results);
 };
 
 export default {
